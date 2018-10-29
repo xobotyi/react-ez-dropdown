223e8bdd490122c44299545f14d82eca541bda20
xobotyi <xog3@yandex.ru>
2018-10-30 01:02:59 +0300

    changes

--- examples/app/components/Body.js
+++ examples/app/components/Body.js
@@ -1,24 +1,15 @@
-import React from "react";
-import {Dropdown, DropdownContent, DropdownTrigger} from "react-ez-dropdown";
+import React                                          from "react";
+import { Dropdown, DropdownContent, DropdownTrigger } from "react-ez-dropdown";
 
-export default class Head extends React.Component {
+export default class Head extends React.Component
+{
     render() {
         return (
-            <div id="AppBody">
-                <div className="packageDescription" />
+                <div id="AppBody">
+                    <div className="packageDescription" />
 
-                <h2>Examples</h2>
-
-                <Dropdown>
-                    <DropdownTrigger>Toggle dropdown</DropdownTrigger>
-                    <DropdownContent className="test1">
-                        <Dropdown>
-                            <DropdownTrigger>Toggle dropdown</DropdownTrigger>
-                            <DropdownContent removeOnHide={false}>helloWorld</DropdownContent>
-                        </Dropdown>
-                    </DropdownContent>
-                </Dropdown>
-            </div>
+                    <h2>Examples</h2>
+                </div>
         );
     }
 }
--- tests/DropdownRegister.spec.js
+++ tests/DropdownRegister.spec.js
@@ -1,4 +1,4 @@
-import expect from "expect";
+import expect           from "expect";
 import DropdownRegister from "../src/util/DropdownRegister";
 
 describe("DropdownRegister", () => {
