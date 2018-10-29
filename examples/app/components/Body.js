9a544aba39f52352e4fc0cd827a2e27ddfff38ee
xobotyi <xog3@yandex.ru>
2018-10-30 01:06:16 +0300

    wrong description;

--- examples/app/components/Body.js
+++ examples/app/components/Body.js
@@ -1,15 +1,14 @@
-import React                                          from "react";
-import { Dropdown, DropdownContent, DropdownTrigger } from "react-ez-dropdown";
+import React from "react";
+import {Dropdown, DropdownContent, DropdownTrigger} from "react-ez-dropdown";
 
-export default class Head extends React.Component
-{
+export default class Head extends React.Component {
     render() {
         return (
-                <div id="AppBody">
-                    <div className="packageDescription" />
+            <div id="AppBody">
+                <div className="packageDescription" />
 
-                    <h2>Examples</h2>
-                </div>
+                <h2>Examples</h2>
+            </div>
         );
     }
 }
